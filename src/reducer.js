192732fd8372399eb33ce57665040e89a8b4f9b2
Erik Metz <erikmetz@wireless-10-104-74-110.umd.edu>
2017-04-08 16:17:06 -0400

    I tried not to get put into vim this time

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,9 @@
 import type {Model} from './././././model';
 import type {Event} from '././event';
 import type {Reducer} from './server/ModelStorage';
+import type {Update} from './update';
+import type {LobbyUpdate} from './update';
+import {makeGameMode} from './././././././././././model';
 
 export const reducer:Reducer = function(event, clientId, model){
   if (model.mode.name === "LobbyMode") {
@@ -15,9 +18,19 @@ const lobbyMode: Reducer = function(event, clientId, model) {
   if (event.data.type === "SetName") {
     let newPlayer = {name: event.data.name, clientId: clientId}
     model.players.set(clientId, newPlayer)
+    return updateAll(model, makeLobbyUpdate(model))
   } else if (event.data.type === "Connect"){
     //bla
+  } else if (event.data.type === "StartGame") {
+    model.mode = makeGameMode()
   }
+}
+
+
+function makeLobbyUpdate(model: Model): LobbyUpdate {
+  let playerNames = []
+  model.players.forEach(function(player, id) {playerNames.push(player.name)})
+  return {name: "LobbyMode", players: playerNames}
 }
 
 const gameMode:Reducer = function(event, clientId, model){
@@ -26,4 +39,12 @@ const gameMode:Reducer = function(event, clientId, model){
   } else if (event.data.type === "Describe"){
     event.data.description;
   }
+}
+
+
+
+function updateAll(model: Model, update: Update): Map<number, Update> {
+  let updateMap = new Map()
+  model.players.forEach(function(player, id) {updateMap.set(id, update)})
+  return updateMap
 }
