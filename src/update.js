0d76786b1ba1550facf0f161cb43fb0f02c74581
Jacob Prinz <jacobeliasprinz@gmail.com>
2017-04-08 14:52:02 -0400

    Update to client type

--- src/update.js
+++ src/update.js
@@ -1,7 +1,29 @@
 // @flow
-import type {Model} from './model';
+// import type {Model} from './model';
+import type {Drawing} from './drawing';
 
 export type Update = {
-  model: Model,
-  lastEventIdFor: Map<number, number>
+}
+
+export type LobbyUpdate = {
+  name: "LobbyMode",
+  players: Array<string>
+}
+
+export type PromptMode = {
+  name: "PromptMode"
+}
+
+export type DescribeMode = {
+  name: "DescribeMode",
+  drawing: Drawing
+}
+
+export type DrawMode = {
+  name: "DrawMode",
+  description: string
+}
+
+export type EndgameMode = {
+
 }
